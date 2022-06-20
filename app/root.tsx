a8f45d5cadd6efcaf731d665aa621851ca369e11
mugi-uno <mugi.uno@gmail.com>
2022-06-20 23:49:04 +0900

    add image and ogp

--- app/root.tsx
+++ app/root.tsx
@@ -10,13 +10,37 @@ import {
 import styles from "./styles/app.css";
 
 export function links() {
-  return [{ rel: "stylesheet", href: styles }];
+  return [
+    { rel: "stylesheet", href: styles },
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      href: "/apple-touch-icon.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      href: "/favicon-32x32.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      href: "/favicon-16x16.png",
+    },
+  ];
 }
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "The world after IE left",
   viewport: "width=device-width,initial-scale=1",
+  "og:title": "The world after IE left",
+  "og:url": "https://the-world-after-ie-left.vercel.app/",
+  "og:image": "https://the-world-after-ie-left.vercel.app/images/icon.png",
+  "og:type": "website",
+  "twitter:card": "summary",
 });
 
 export default function App() {
